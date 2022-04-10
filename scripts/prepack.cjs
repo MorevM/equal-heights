f00ea4827f396ba842d14141c72d5ed05a505292
MorevM <max.seainside@gmail.com>
2022-04-10 21:34:36 +0300

    fix: Correct filename for `postinstall` script

--- scripts/prepack.cjs
+++ scripts/prepack.cjs
@@ -5,5 +5,5 @@ const packageJsonContents = fs.readFileSync('package.json', { encoding: 'utf8' }
 fs.writeFileSync(
 	'package.json',
 	packageJsonContents
-		.replace('"scripts": {', '"scripts": {\n\t\t"postinstall": "node ./scripts/postinstall.js",'),
+		.replace('"scripts": {', '"scripts": {\n\t\t"postinstall": "node ./scripts/postinstall.cjs",'),
 );
