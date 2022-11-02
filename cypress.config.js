ea84c0552aa730ae0fcf6174bbe755d123cac360
MorevM <max.seainside@gmail.com>
2022-11-02 18:58:43 +0300

    test: Fix tests to fit updated Cypress v10

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+	fixturesFolder: './__tests__/__fixtures__',
+	screenshotsFolder: './tmp/cypress/screenshots',
+	downloadsFolder: './tmp/cypress/downloads',
+	videosFolder: './tmp/cypress/videos',
+	video: false,
+	viewportWidth: 1280,
+	viewportHeight: 720,
+	e2e: {
+		setupNodeEvents(on, config) {},
+		baseUrl: 'http://localhost:3001',
+		specPattern: './__tests__/integration/tests/**/*.cy.{js,jsx,ts,tsx}',
+		supportFile: './__tests__/integration/bootstrap.js',
+	},
+});
